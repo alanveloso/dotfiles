352e5c3647a3edf888018e22e89bcfa8d20aafac
Alan Veloso <alantsv@gmail.com>
2019-04-25 21:26:43 -0300

    Added chrome installation

--- install.sh
+++ install.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
-sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt update
+sudo apt upgrade -y
+
+if ! hash google-chrome-stable 2>/dev/null; then
+  echo "Adding chrome source list"
+  sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+  wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+  sudo apt update
+else
+  echo "chrome already installed";
+fi
 
 echo "Installing apt packages"
 
-  sudo apt-get -y install git /
+  sudo apt install -y git /
     vim /
     snapd /
+    google-chrome-stable
 
 echo "Installing snap packages"
   sudo snap install spotify
+  sudo snap install --classic code
