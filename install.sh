01afe0333c4841e0e994a446ef15bcbf9a26172f
Alan Veloso <alantsv@gmail.com>
2021-10-20 10:00:54 -0300

    Change spotify installing method

--- install.sh
+++ install.sh
@@ -12,14 +12,18 @@ else
   echo "chrome already installed";
 fi
 
+if ! hash spotify 2>/dev/null; then
+  echo "Adding spotify source list"
+  curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
+  sudo sh -c 'echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list'
+  sudo apt update
+else
+  echo "spotify already installed";
+fi
+
 echo "Installing apt packages";
 
-sudo apt install -y git vim snapd google-chrome-stable telegram-desktop
-
-echo "Installing snap packages"
-
-sudo snap install spotify
-sudo snap install --classic code
+sudo apt install -y git vim snapd google-chrome-stable telegram-desktop spotify
 
 echo "Installing github packages"
 
