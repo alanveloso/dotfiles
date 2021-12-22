e929224dc68539b6146e0a439c0d7bb50283f18b
Alan Veloso <alantsv@gmail.com>
2021-12-22 16:38:55 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,10 +26,23 @@ else
 fi
 
 if ! hash telegram-desktop 2>/dev/null; then
+  echo "Installing telegram-desktop"
   wget -O- https://telegram.org/dl/desktop/linux | sudo tar xJ -C /opt/
   sudo ln -s /opt/Telegram/Telegram /usr/local/bin/telegram-desktop
 else
   echo "telegram-desktop already installed"
+fi
+
+if ! hash code 2>/dev/null; then
+  echo "Installing code"
+  wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+  sudo install -o root -g root -m 644 packages.microsoft.gpg /etc/apt/trusted.gpg.d/
+  sudo sh -c 'echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/trusted.gpg.d/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" > /etc/apt/sources.list.d/vscode.list'
+  rm -f packages.microsoft.gpg
+  sudo apt install apt-transport-https
+  sudo apt update && sudo apt install code
+else
+  echo "code already installed"
 fi
 
 echo "Installing github packages"
