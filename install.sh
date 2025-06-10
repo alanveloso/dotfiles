f80b012e193024438d5191dad8d23ab94d3d12c5
Alan Veloso <alantsv@gmail.com>
2025-06-10 17:05:13 -0300

    remove: Chrome install

--- install.sh
+++ install.sh
@@ -7,15 +7,6 @@ echo "Installing apt packages";
 
 sudo apt install -y git vim curl
 
-if ! hash google-chrome-stable 2>/dev/null; then
-  echo "Installing google-chrome"
-  sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-  wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-  sudo apt update && sudo apt install google-chrome-stable -y
-else
-  echo "chrome already installed";
-fi
-
 if ! hash spotify 2>/dev/null; then
   echo "Installing spotify"
   curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | sudo apt-key add - 
